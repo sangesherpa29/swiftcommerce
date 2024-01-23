4a5ad7c3f2f3c64aca9585213f6c70bc0f2d6eda
iSangeSherpa <sangesherpa1215@gmail.com>
2024-01-23 12:00:59 +0545

    Initial Commit

---
+++ SwiftCommerce/SwiftCommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCommerceApp.swift
+//  SwiftCommerce
+//
+//  Created by Web and App on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
